c7a6348f47ec348009eb69b75c4f7daf99f5a438
VinayProgram <vinaytandale@gmail.com>
2026-06-02 17:09:54 +0530

    spacial tracking updated vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    basicSsl()
   ],
   server: {
     headers: {

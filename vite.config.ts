9c5b235412f338daecc8f0ea290c6110674958b7
VinayProgram <vinaytandale@gmail.com>
2026-06-04 15:07:07 +0530

    spacial tracking updated vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
-    basicSsl()
+    basicSsl(),
   ],
+
+  server: {
+    headers: {
+      'Permissions-Policy':
+        'camera=*, microphone=*, xr-spatial-tracking=*'
+    }
+  },
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
-
-
